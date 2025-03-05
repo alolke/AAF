09376ad83a10527eb79306e4a7062ee261d7d9aa
alolke <116136990+alolke@users.noreply.github.com>
2025-03-05 23:31:20 +0800

    Update Regulator.py

--- AAF/Regulator.py
+++ AAF/Regulator.py
@@ -9,32 +9,32 @@ def Regulator(eval_results, adjustable_para, stop_threshold):
     print(f"{loss_2}, {rouge_2}")
 
     Stop_symbol = 0
-    # 比较结果
+    # Comparison
     if loss_2 < loss_1 and rouge_2 > rouge_1:
-        print("操作：增加多样性增强数据")
+        print("Operation: Increase diversity enhancement number")
         if adjustable_para >= -0.3 and adjustable_para <= 0.3:
             adjustable_para += 0.1
             Stop_symbol = 0
 
     elif loss_2 > loss_1 and rouge_2 > rouge_1:
-        print("操作：增加简单增强数据")
+        print("Operation: Add simple enhanced data")
         if adjustable_para >= -0.3 and adjustable_para <= 0.3:
             adjustable_para -= 0.1
             Stop_symbol = 0
     elif loss_2 < loss_1 and rouge_2 < rouge_1:
-        print("操作：增加简单增强数据")
+        print("Operation: Add simple enhanced data")
         if adjustable_para >= -0.3 and adjustable_para <= 0.3:
             adjustable_para -= 0.1
             Stop_symbol = 0
     elif loss_2 > loss_1 and rouge_2 < rouge_1:
-        print("效果降低, 过拟合")
+        print("Reduced effectiveness and overfitting")
         if adjustable_para >= -0.3 and adjustable_para <= 0.3:
             adjustable_para -= 0.1
             stop_threshold += 1
             Stop_symbol = 1
 
     else:
-        print("未定义的情况")
+        print("Undefined situation")
 
     return adjustable_para, Stop_symbol, stop_threshold
 
